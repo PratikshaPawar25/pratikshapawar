51cb6b2716777c0532a2e5abdb827a26c9df080d
PratikshaPawar25 <pratikshapawar252005@gmail.com>
2025-06-03 19:26:09 +0530

    Initial website commit

---
+++ web/style.css
@@ -0,0 +1,41 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background: #f0f0f5;
+    color: #333;
+  }
+  
+  header {
+    background: #2c3e50;
+    color: #fff;
+    padding: 2rem;
+    text-align: center;
+  }
+  
+  section {
+    padding: 2rem;
+    max-width: 800px;
+    margin: auto;
+  }
+  
+  footer {
+    text-align: center;
+    padding: 1rem;
+    background: #2c3e50;
+    color: white;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
+  
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  
+  ul li::before {
+    content: "• ";
+    color: #3498db;
+  }
+  
